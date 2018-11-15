a9ab0800a2223980b049ca2eb118a984714ccfef
Barsya Prastoro <barsya96@gmail.com>
2018-11-15 09:44:06 +0700

    gagal

--- application/controllers/C_API.php
+++ application/controllers/C_API.php
@@ -100,7 +100,7 @@ class C_API extends CI_Controller {
 			]);
 		}
 
-		 
+
 	}
 
 	// API Menu home pembeli
@@ -204,7 +204,9 @@ class C_API extends CI_Controller {
 
 			$menu = $this->Menu->all(['idMenu','namaMenu','fotoMenu','deskripsiMenu','hargaMenu'],'namaMenu','asc',[
 				'idPedagang' => $idPedagang
+
 			]);
+
 			
 			echo json_encode($menu);
 		}else{
@@ -287,56 +289,6 @@ class C_API extends CI_Controller {
 		}
 	}
 
-	//transaksi
-
-	function pesanMenu(){
-		if($this->input->method() != "post") return;
-		header('Content-Type: application/json');
-		$this->load->model('Transaksi');
-		$this->load->library('auth');
-		$this->load->model('User');
-
-		$token = $this->input->get_request_header('Authorization', true);
-
-		if($this->auth->isAuthUser($token)) {
-			// $menu = $this->Menu->one($id);
-			// header('Content-Type: application/json');
-			// echo json_encode($menu);
-			
-
-			$data = [];
-			
-			log_message('error', "before"); 
-			$req = json_decode( file_get_contents('php://input') );
-			log_message('error', "after");
-			$data['namaMenu'] = $req->namaMenu;
-			$data['hargaMenu'] = $req->hargaMenu;
-			$data['deskripsiMenu'] = $req->deskripsiMenu;
-			$data['fotoMenu'] = $req->fotoMenu;
-
-			$username = $this->auth->getUserByToken($token);
-
-			$userdata = $this->user->getByUser($username);
-
-			$data['idPedagang'] = $userdata->idUser;
-
-			//$this->db->trans_begin();
-
-			log_message('error', "before sql");
-			$resdb = $this->menu->insert($data);
-			log_message('error', "after sql");
-			$idMenu = $this->db->insert_id();
-			//$this->db->trans_commit();
-			echo json_encode([
-				"status" => "OK"        
-			]);
-		}
-		else{
-			echo json_encode([
-				"status" => "NOK",
-				"message" => "Invalid Token"        
-			]);
-		}
-	}
+	
 
 }
