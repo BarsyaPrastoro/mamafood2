5a84b0c8828c32eaa5a951624b31396fd4abe437
Tobias Jaya Sutjipto <tobiasjaya3@gmail.com>
2018-11-23 21:29:26 +0700

    merge?

--- application/views/divisi/profitablemeasure/pm-laporanbelisaldo.php
+++ application/views/divisi/profitablemeasure/pm-laporanbelisaldo.php
@@ -3,32 +3,26 @@
     <!-- <link rel="stylesheet" href="../../asset/css/style.css"> -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <<<<<<< HEAD
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
-    =======
+    
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
     
-    >>>>>>> e8f00ed775bbd49ac3f85702accb4dbf3b3d0ed7
-
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
-    <<<<<<< HEAD
 
 
-    =======
-    
-    >>>>>>> e8f00ed775bbd49ac3f85702accb4dbf3b3d0ed7
 </head>
 <body>
     <?= $topbar ?>
