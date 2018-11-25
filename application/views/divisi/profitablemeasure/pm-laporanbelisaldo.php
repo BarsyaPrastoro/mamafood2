9d2c738c417be1a959d3c42d04d8f7f370b947fc
Barsya Prastoro <barsya96@gmail.com>
2018-11-25 17:31:40 +0700

    laporan beli saldo

--- application/views/divisi/profitablemeasure/pm-laporanbelisaldo.php
+++ application/views/divisi/profitablemeasure/pm-laporanbelisaldo.php
@@ -157,30 +157,28 @@
                                             <div class="col-md-5">
                                                 <img class="fotoktpdape" src= "data:image/jpeg;base64, <?= $row->bukti_transfer?>">
                                             </div>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                              <span aria-hidden="true">&times;</span>
-                                          </button>
-                                      </div>
-                                      
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            
+                                        </div>
                                         
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+
+            </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-lg-12">
-
-        </div>
-    </div>
-</div>
 </div>
 
 </div>
